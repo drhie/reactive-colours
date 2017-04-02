ad8568da6ca874b5b6062099443d1d60130a8d4c
drhie <rhie.david@gmail.com>
2017-04-02 19:34:40 -0400

    Create event handler that is recorded from the parent container's side

--- src/BoxShell.js
+++ src/BoxShell.js
@@ -1,5 +1,5 @@
 var React = require('react')
-var Box = require('./Box')
+// var Box = require('./Box')
 
 var boxShellStyles = {
   width: 602,
@@ -7,21 +7,57 @@ var boxShellStyles = {
   display: 'inline-block'
 }
 
+var boxStyles = {
+  width: 100,
+  height: 100,
+  border: '1px solid black',
+  display: 'inline-block',
+  margin: 0,
+  padding: 0,
+  background: 'beige',
+}
+
+var boxTextStyle = {
+  'vertical-align': 'middle',
+
+}
+
+function Box(props) {
+  return (
+    <div style={boxStyles} onClick={() => props.onClick()}>
+      {props.value}
+    </div>
+  );
+}
+
 class BoxShell extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      boxes: Array(6).fill(null)
+    }
+  }
   handleClick(i) {
+    const boxes = this.state.boxes.slice();
+    boxes[i] = "This Box has been Clicked!";
     this.setState({
-      text: 'This box has been clicked!'
+      boxes: boxes
     });
   }
+  renderBox(i) {
+    return <Box value={this.state.boxes[i]} onClick={() => this.handleClick(i)} />;
+  }
   render() {
-    // var arrayOfBoxes = [];
-    // // for (var i=0; i<9; i++) {
-    // //   arrayOfBoxes.push(<Box value={i+1} text={i+1} onClick={() => this.handleClick(i)} />)
-    // // };
-
     return(
-      <div style={boxShellStyles}>
-        <Box value={1} text={1} onClick={() => this.handleClick()} />
+      <div>
+
+        {this.renderBox(0)}
+        {this.renderBox(1)}
+        {this.renderBox(2)}
+        {this.renderBox(3)}
+        {this.renderBox(4)}
+        {this.renderBox(5)}
+
       </div>
     )
   }
