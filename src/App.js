ab57eef4409d9e94711c70037efa1402cb56afc8
drhie <rhie.david@gmail.com>
2017-04-02 14:04:08 -0400

    Create a component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// var ReactDOM = require('react-dom')
+
+var Box = require('./Box')
+
 class App extends Component {
   render() {
     return (
@@ -13,9 +17,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Box />
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/Box.js
@@ -0,0 +1,13 @@
+var React = require('react')
+
+var Box = React.createClass({
+  render: function() {
+    return (
+      <div>
+        This is a box.
+      </div>
+    )
+  }
+})
+
+module.exports = Box
